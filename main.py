6e5191c34e091ff1c1a181c5e9f398eef376c10b
uribo <ur1b0.dev@gmail.com>
2022-01-26 19:25:18 +0900

    とりあえず動く

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# python3.7で動作確認済み
+
+import serial
+import time
+import json
+import datetime
+import os
+
+DEBUG = True
+
+def logging(path, name, data):
+    timestamp = datetime.datetime.now()
+    filename = name + "_" + timestamp.strftime("%Y-%m-%d") + ".csv"
+    write_str = timestamp.strftime("%Y/%m/%d %H:%M:%S") + "," + data
+    path = path + "/" + name + "/"
+
+    os.makedirs(path, exist_ok=True)
+    f = open(path + filename, mode="a")
+    f.write(write_str + "\n")
+    f.close()
+
+# 設定値読み込み
+f = open("/home/pi/work/logger_usb_common_sense/config.json", "r")
+conf = json.loads(f.read())
+f.close()
+
+path = conf["basedir"] + "/" + conf["logdir_name"]
+
+for device in conf["devices"]:
+    readSer = serial.Serial(
+        device["serial_port"], device["serial_rate"], timeout=3)
+    raw = readSer.readline().decode().replace('\n', '')
+    print(raw)
+    readSer.close()
+    logging(path, device["sensor_name"], raw)
